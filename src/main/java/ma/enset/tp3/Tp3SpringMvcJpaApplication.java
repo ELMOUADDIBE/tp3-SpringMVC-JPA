dda00561fb287de4c775c39dc8d31fbe2c39eaac
EL MOUADDIBE <elmouaddibezaid@gmail.com>
2024-03-10 14:27:10 +0000

    init

---
+++ src/main/java/ma/enset/tp3/Tp3SpringMvcJpaApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3SpringMvcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3SpringMvcJpaApplication.class, args);
+	}
+
+}
